e84c0482e9e604d44c47bc0fe324e7e67a27c284
Chiptune93 <eoen012@gmail.com>
2023-04-08 20:27:13 +0900

    feature: file upload commit#1

---
+++ src/main/java/dev/chiptune/springboot/repository/FileUploadRepository.java
@@ -0,0 +1,19 @@
+package dev.chiptune.springboot.repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileUploadRepository {
+    int getMasterSeq();
+
+    List<HashMap<String, String>> fileList(HashMap<String, String> parameterMap);
+
+    int insertFile(HashMap<String, String> parameterMap);
+
+    int deleteFile(HashMap<String, String> parameterMap);
+
+    HashMap<String, String> info(int seq);
+}
