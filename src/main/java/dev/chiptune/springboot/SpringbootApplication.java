d1c77a3b240ab7e490548ff207decf4cdb4abab0
Chiptune93 <eoen012@gmail.com>
2023-04-29 23:49:12 +0900

    feature: thymeleaf#2

--- src/main/java/dev/chiptune/springboot/SpringbootApplication.java
+++ src/main/java/dev/chiptune/springboot/SpringbootApplication.java
@@ -1,12 +1,14 @@
 package dev.chiptune.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(scanBasePackages = {"dev.chiptune.springboot.**"})
+@SpringBootApplication
+@MapperScan(basePackageClasses = SpringbootApplication.class)
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
